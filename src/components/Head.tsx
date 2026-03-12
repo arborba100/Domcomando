de20f6bfb6458093bde71d4e9c511efc6a53b27e
Wix Remote Machine <support@wix.com>
2026-03-12 15:51:30 +0000

    Fix Blank Website Display

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,6 +5,9 @@ export const Head = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
+      {/* Font preloads */}
+      <link rel="preload" href="/fonts/leaguespartan/v15/kJEqBuEW6A0lliaV_m88ja5TwvZwLZmXD4Zh.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+      <link rel="preload" href="/fonts/spacegrotesk/v22/V8mDoQDjQSkFtoMM3T6r8E7mPbF4C_k3HqU.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
     </>
   );
 };
