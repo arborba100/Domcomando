07df4a02508f1e4f3ed685091f8f980b70d8e108
Wix Remote Machine <support@wix.com>
2026-03-19 07:48:31 +0000

    Ajustar background para 90%

--- src/components/pages/LuxuryShowroomPage.tsx
+++ src/components/pages/LuxuryShowroomPage.tsx
@@ -6,18 +6,20 @@ export default function LuxuryShowroomPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex-1 relative overflow-hidden">
-        {/* Full-screen background image - fixed to fill remaining space */}
-        <Image
-          src="https://static.wixstatic.com/media/50f4bf_38c9f88d54654e38906e049af6a8b5a4~mv2.png"
-          alt="Luxury Showroom Background"
-          className="w-full h-full object-cover"
-          width={1920}
-          height={1080}
-        />
-        
-        {/* Overlay for content */}
-        <div className="absolute inset-0 bg-black/20" />
+      <div className="flex-1 flex items-center justify-center py-4">
+        {/* Background image occupying 90% of screen height */}
+        <div className="relative w-full h-[90vh] overflow-hidden rounded-lg">
+          <Image
+            src="https://static.wixstatic.com/media/50f4bf_38c9f88d54654e38906e049af6a8b5a4~mv2.png"
+            alt="Luxury Showroom Background"
+            className="w-full h-full object-cover"
+            width={1920}
+            height={1080}
+          />
+          
+          {/* Overlay for content */}
+          <div className="absolute inset-0 bg-black/20" />
+        </div>
       </div>
       <Footer />
     </div>
