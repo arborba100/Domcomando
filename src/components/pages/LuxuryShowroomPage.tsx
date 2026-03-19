1110244ffd807bfe42daec58366a8e9159854593
Wix Remote Machine <support@wix.com>
2026-03-19 06:54:38 +0000

    Restored to checkpoint afc0aeaa922cfc9a5168ef9cf3df0fe514cb22eb

--- src/components/pages/LuxuryShowroomPage.tsx
+++ src/components/pages/LuxuryShowroomPage.tsx
@@ -1,15 +1,20 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import Luxury3DShowroom from '@/components/Luxury3DShowroom';
+import { Image } from '@/components/ui/image';
 
 export default function LuxuryShowroomPage() {
   return (
     <div className="w-screen h-screen overflow-hidden flex flex-col">
       <Header />
       
-      {/* 3D Showroom Section - Full Screen */}
+      {/* Background Section - Full Screen */}
       <div className="flex-1 w-full h-full">
-        <Luxury3DShowroom />
+        <Image
+          src="https://static.wixstatic.com/media/50f4bf_8787d9f97cfa4afe99b4bdd843fde7da~mv2.png"
+          alt="Luxury Showroom Background"
+          className="w-full h-full object-cover"
+          width={1600}
+        />
       </div>
 
       <Footer />
