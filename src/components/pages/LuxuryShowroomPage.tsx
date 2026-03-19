afc0aeaa922cfc9a5168ef9cf3df0fe514cb22eb
Wix Remote Machine <support@wix.com>
2026-03-19 06:21:34 +0000

    Preencher tela com background

--- src/components/pages/LuxuryShowroomPage.tsx
+++ src/components/pages/LuxuryShowroomPage.tsx
@@ -4,19 +4,17 @@ import { Image } from '@/components/ui/image';
 
 export default function LuxuryShowroomPage() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="w-screen h-screen overflow-hidden flex flex-col">
       <Header />
       
-      {/* Background Section */}
-      <div className="flex-1 w-full flex items-center justify-center p-4">
-        <div className="w-full h-[90vh] max-w-[90%]">
-          <Image
-            src="https://static.wixstatic.com/media/50f4bf_8787d9f97cfa4afe99b4bdd843fde7da~mv2.png"
-            alt="Luxury Showroom Background"
-            className="w-full h-full object-contain"
-            width={1600}
-          />
-        </div>
+      {/* Background Section - Full Screen */}
+      <div className="flex-1 w-full h-full">
+        <Image
+          src="https://static.wixstatic.com/media/50f4bf_8787d9f97cfa4afe99b4bdd843fde7da~mv2.png"
+          alt="Luxury Showroom Background"
+          className="w-full h-full object-cover"
+          width={1600}
+        />
       </div>
 
       <Footer />
